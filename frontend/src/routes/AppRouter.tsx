7e6073aefb92f302d29bbf3f68c08860946ae7cc
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-25 14:50:41 -0300

    refactor: improve user access on private routes

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,22 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router";
 import { Layout } from "@components/layout/Layout";
 import { Welcome } from "@components/sections/Welcome/Welcome";
 import { MedicosDisponiblesSection } from "@components/sections/MedicosDisponibles/MedicosDisponiblesSection";
 import { AgendaTuCitaSection } from "@components/sections/Paciente/AgendaTuCita/AgendaTuCitaSection";
-import { PacienteGuard } from "@routes/PacienteGuard";
-import { AdminGuard } from "@routes/AdminGuard";
-import { MedicoGuard } from "@routes/MedicoGuard";
 import { NotFound } from "@components/layout/NotFound/NotFound";
 import { DoctorDetails } from "@components/sections/MedicosDisponibles/DoctorDetails/doctorDetails";
 import { DashboardPatient } from "@components/sections/Paciente/DashboardPatient/DashboardPatient";
 import { DashboardMedico } from "@components/sections/Medico/DashboardMedico/DashboardMedico";
 import { GestionarPacientes } from "@components/sections/Medico/GestionarPacientes/GestionarPacientes/GestionarPacientes";
-import { ContenidoDatosDiagnostico } from "@components/sections/Paciente/DashboardPatient/ContenidoDatosDiagnostico/ContenidoDatosDiagnostico"
+import { ContenidoDatosDiagnostico } from "@components/sections/Paciente/DashboardPatient/ContenidoDatosDiagnostico/ContenidoDatosDiagnostico";
 import { ContenidoHistorialCitas } from "@components/sections/Paciente/DashboardPatient/ContenidoHistorialCitas/ContenidoHistorialCitas";
 import { HistorialMedico } from "@components/sections/Medico/HistorialMedico/HistorialMedico";
 import { GestionarAgenda } from "@components/sections/Medico/GestionarAgenda/GestionarAgenda";
 import { GestionarMedicamentos } from "@components/sections/Medico/GestionarPacientes/GestionarMedicamentos/GestionarMedicamentos";
 import { DashboardAdmin } from "@components/sections/Admin/DashboardAdmin";
+import { ProtectedRoute } from "@routes/ProtectedRoute";
+import { UserRole } from "@tipos/store";
+import { BrowserRouter, Route, Routes } from "react-router";
 
 export const AppRouter = () => {
     return (
@@ -24,21 +23,24 @@ export const AppRouter = () => {
             <Routes>
                 <Route element={<Layout />}>
                     <Route path="*" element={<NotFound />} />
+                    <Route path="/" element={<Welcome />} />
                     <Route path="/medicos-disponibles" element={<MedicosDisponiblesSection />} />
                     <Route path="/medico/:id" element={<DoctorDetails />} />
                     <Route path="/agendar-cita" element={<AgendaTuCitaSection />} />
-                    <Route path="/" element={<Welcome />} />
-                    <Route element={<PacienteGuard />}>
+
+                    <Route element={<ProtectedRoute requiredRole={UserRole.PACIENTE} />}>
                         <Route path="/paciente/dashboard" element={<DashboardPatient />} />
                         <Route path="/paciente/dashboard/settings" element={<>Settings</>} />
                         <Route path="/paciente/datos-diagnostico" element={<ContenidoDatosDiagnostico />} />
                         <Route path="/paciente/historial-citas" element={<ContenidoHistorialCitas />} />
                         <Route path="/paciente/agendar-cita" element={<AgendaTuCitaSection />} />
                     </Route>
-                    <Route element={<AdminGuard />}>
+
+                    <Route element={<ProtectedRoute requiredRole={UserRole.ADMIN} />}>
                         <Route path="/admin/dashboard" element={<DashboardAdmin />} />
                     </Route>
-                    <Route element={<MedicoGuard />}>
+
+                    <Route element={<ProtectedRoute requiredRole={UserRole.MEDICO} />}>
                         <Route path="/medico/dashboard" element={<DashboardMedico />} />
                         <Route path="/medico/historial-citas" element={<HistorialMedico />} />
                         <Route path="/medico/gestionar-pacientes" element={<GestionarPacientes />} />
@@ -49,4 +51,4 @@ export const AppRouter = () => {
             </Routes>
         </BrowserRouter>
     );
-}
+};
