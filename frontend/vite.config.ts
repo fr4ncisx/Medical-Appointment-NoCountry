3aee73f09df9508f59ace8f18a04aff9465c61e0
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-03-27 14:58:24 -0300

    refactor: se removio un alias innecesario

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   plugins: [react(),tsConfigPaths()],
   resolve: {
     alias: {
-      '@assets/*': "/src/assets/*",
       '@components/*': "/src/components/*",
       '@ui/*': "/src/ui/*",
       '@utils/*': "/src/utils/*",
