924a46e677d31addffe55229c0500d5aedd9a089
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-04-02 10:18:02 -0300

    feat: add new custom render

--- frontend/src/ui/Form/JsonFormTesters.tsx
+++ frontend/src/ui/Form/JsonFormTesters.tsx
@@ -1,7 +1,22 @@
-import { rankWith, scopeEndsWith } from "@jsonforms/core";
+import { and, rankWith, schemaMatches, schemaTypeIs, scopeEndsWith, uiTypeIs } from "@jsonforms/core";
 
 export const passwordConfirmControlTester = rankWith(3, scopeEndsWith("repeatPassword"));
 export const customDatePickerControlTester = rankWith(3, scopeEndsWith("customDatePicker"));
 export const customSelectControlTester = rankWith(3, scopeEndsWith("customSelect"));
 export const selectorMedicosTester = rankWith(3, scopeEndsWith("selectorMedicos"));
 export const datePickerWithRangeTester = rankWith(3, scopeEndsWith("datePickerWithRange"));
+export const timePickerWithRangeTester = rankWith(
+    100,
+    and(
+        uiTypeIs('Control'),
+        schemaTypeIs("object"),
+        schemaMatches((schema) => {
+            if(Object.prototype.hasOwnProperty.call(schema, 'customRender')) {
+                const cellschema = schema as { customRender?: string };
+                return cellschema.customRender === "TimePickerWithRange"
+            } 
+            return false;
+        }),
+    )
+);
+
