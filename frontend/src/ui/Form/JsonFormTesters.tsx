bc9f262f0475bb4f5447584ab2a69a3a2ac29235
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-04-15 16:10:25 -0300

    refactor: se mejoro el custom render PasswordConfirmControl para mejorar su reutilizacion

--- frontend/src/ui/Form/JsonFormTesters.tsx
+++ frontend/src/ui/Form/JsonFormTesters.tsx
@@ -1,7 +1,22 @@
 import { and, rankWith, schemaMatches, schemaTypeIs, scopeEndsWith, uiTypeIs } from "@jsonforms/core";
 
-export const passwordConfirmControlTester = rankWith(3, scopeEndsWith("repeatPassword"));
 export const selectorMedicosTester = rankWith(3, scopeEndsWith("selectorMedicos"));
+
+export const passwordConfirmControlTester = rankWith(
+    3,
+    and(
+        uiTypeIs('Control'),
+        schemaTypeIs("object"),
+        schemaMatches((schema) => {
+            if (Object.prototype.hasOwnProperty.call(schema, 'customRender')) {
+                const cellschema = schema as { customRender?: string };
+                return cellschema.customRender === "PasswordConfirmControl"
+            }
+            return false;
+        }),
+    )
+);
+
 export const datePickerWithRangeTester = rankWith(
     3,
     and(
