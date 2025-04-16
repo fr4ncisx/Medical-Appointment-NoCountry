7377dd1656f6ed01496a30fe891f3bc4fd9e5146
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-04-16 10:22:10 -0300

    refactor: se dividio el componente Detail en mas componentes

--- frontend/src/components/sections/Welcome/ContentBox/Details/Details.tsx
+++ frontend/src/components/sections/Welcome/ContentBox/Details/Details.tsx
@@ -1,8 +1,7 @@
-import { Box, Typography, IconButton } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { DetailsStyle } from "./DetailsStyle";
-import StethoscopeIcon from "./StethoscopeIcon";
-import HospitalIcon from "./HospitalIcon";
-import CoinIcon from "./CoinIcon";
+import { SECTIONS } from "./SECTIONS";
+import { Section } from "./Section";
 
 export const Details = () => {
   return (
@@ -10,61 +9,11 @@ export const Details = () => {
       <Typography variant="h4" sx={DetailsStyle.sectionTitle}>
         Nuestra prioridad es la salud de nuestros clientes
       </Typography>
-
-      <Box sx={DetailsStyle.sectionContainer}>
-        <Box sx={DetailsStyle.iconContainer}>
-          <IconButton>
-            <HospitalIcon />
-          </IconButton>
-        </Box>
-
-        <Box>
-          <Typography variant="subtitle1" fontWeight="bold" color="white">
-            Modernización clínica
-          </Typography>
-          <Typography variant="body1" color="white">
-            Accede a una vasta lista de profesionales de la salud, agenda tu
-            cita en minutos, chatea con tu médico de cabecera, nos adecuamos a
-            tus necesidades.
-          </Typography>
-        </Box>
-      </Box>
-
-      <Box sx={DetailsStyle.sectionContainer}>
-        <Box sx={DetailsStyle.iconContainer}>
-          <IconButton>
-            <CoinIcon />
-          </IconButton>
-        </Box>
-        <Box>
-          <Typography variant="subtitle1" fontWeight="bold" color="white">
-            Sin comisiones
-          </Typography>
-          <Typography variant="body1" color="white">
-            No cobramos comisiones por agendar una cita médica, lo único que
-            debes abonar es la cita con el médico que se adecúe a tus
-            necesidades.
-          </Typography>
-        </Box>
-      </Box>
-
-      <Box sx={DetailsStyle.sectionContainer}>
-        <Box sx={DetailsStyle.iconContainer}>
-          <IconButton>
-            <StethoscopeIcon />
-          </IconButton>
-        </Box>
-        <Box>
-          <Typography variant="subtitle1" fontWeight="bold" color="white">
-            Equipo de profesionales
-          </Typography>
-          <Typography variant="body1" color="white">
-            Contamos con un equipo médico profesional que fue formado en
-            universidades de prestigio y verificados por nuestros asistentes
-            administrativos.
-          </Typography>
-        </Box>
-      </Box>
+      {
+        SECTIONS.map(section => (
+          <Section {...section} />
+        ))
+      }
     </Box>
   );
 };
