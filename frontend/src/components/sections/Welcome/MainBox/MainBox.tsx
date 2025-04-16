16782feaf62d47ae3f370ea2be5409729aa74b6f
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-04-16 15:46:32 -0300

    feat: se cambio el estilo del MainBox y se su visualizacion en dispositivos moviles

--- frontend/src/components/sections/Welcome/MainBox/MainBox.tsx
+++ frontend/src/components/sections/Welcome/MainBox/MainBox.tsx
@@ -1,23 +1,48 @@
 import { Box, Typography } from "@mui/material";
 import { AgendarCitaButton } from "./AgendarCitaButton";
-import { MainBoxStyles } from "./MainBoxStyles";
 
 export const MainBox = () => {
-    return (
-        <Box sx={MainBoxStyles.mainBox}>
-            <Box sx={MainBoxStyles.welcomeBox}>
-                <Box sx={MainBoxStyles.circle1} />
-                <Box sx={MainBoxStyles.circle2} />
-                <Box>
-                    <Typography sx={MainBoxStyles.title}>
-                        MedicalAppointment
-                    </Typography>
-                    <Typography sx={MainBoxStyles.subtitle}>
-                        Tu lugar para pedir un turno
-                    </Typography>
-                </Box>
-                <AgendarCitaButton />
-            </Box>
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "650px",
+        backgroundColor: "#DADADA",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        width={"min-content"}
+        gap={"2em"}
+      >
+        <Box>
+          <Typography
+            fontSize={"clamp(1em, 10vw, 4em)"}
+            lineHeight="1"
+            margin="0"
+            fontWeight="bold"
+            color={"#185D3B"}
+          >
+            Bienvenido a MedicalAppointment
+          </Typography>
+          <Typography
+            variant="h5"
+            fontSize={"clamp(.5em, 10vw, 1.5em)"}
+            mt={"1rem"}
+            color={"#198751"}
+          >
+            Agenda tu cita cuando quieras o donde quieras
+          </Typography>
         </Box>
-    );
-}
+        <Box>
+          <AgendarCitaButton />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
