97676dc99da80cf619e7b725738468e5161d7d7d
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-04-21 15:53:14 -0300

    feat: se mejoro el responsive del componente Footer

--- frontend/src/components/layout/Footer/FooterStyle.ts
+++ frontend/src/components/layout/Footer/FooterStyle.ts
@@ -4,13 +4,14 @@ export const FooterStyle: ObjectStyles = {
   footerContainer: {
     width: "100%",
     backgroundColor: "#f1f1f1",
-    padding: "20px 0",
+    padding: "20px 5vw",
     display: "flex",
     justifyContent: "space-around",
     borderTop: "1px solid #c1c1c1",
     flexWrap: "wrap",
+    gap: "1em",
     alignItems: "center",
-    height: "230px"
+    minHeight: "200px",
   },
   footerTitle: {
     display: "flex",
@@ -30,34 +31,34 @@ export const FooterStyle: ObjectStyles = {
   footerLinksSectionTitle: {
     color: "#198751",
     fontSize: "18px",
-    cursor: "default"
+    cursor: "default",
   },
   footerSocial: {
     textAlign: "center",
   },
   footerSocialTitle: {
     color: "#198751",
-    fontSize: "18px"
+    fontSize: "18px",
   },
   footerLink: {
     fontSize: "16px",
     lineHeight: "1.2",
     ":hover": {
-      textDecoration: "underline"
-    }
+      textDecoration: "underline",
+    },
   },
   footerIconsContainer: {
     display: "flex",
-    gap: "0.5rem"
+    gap: "0.5rem",
   },
   footerButton: {
     backgroundColor: "#198751",
     "&:hover": {
       backgroundColor: "#19875180",
-    }
+    },
   },
   footerIcons: {
     color: "#f1f1f1",
     borderRadius: "50%",
-  }
+  },
 };
