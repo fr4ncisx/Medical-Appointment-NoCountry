3d9b8a10e145b50ea61098eba81fe5b7b8906b32
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-04-22 11:51:05 -0300

    feat: se mejoro el responsive de la vista medicos disponibles

--- frontend/src/components/sections/MedicosDisponibles/MedicosDisponiblesSection.tsx
+++ frontend/src/components/sections/MedicosDisponibles/MedicosDisponiblesSection.tsx
@@ -6,35 +6,35 @@ import CustomModal from "@ui/CustomModal/CustomModal";
 import ModalMenu from "../Welcome/ModalMenu/ModalMenu";
 
 export const MedicosDisponiblesSection = () => {
-    const showModal = useModalStore((state) => state.modalData.showModal);
+  const showModal = useModalStore((state) => state.modalData.showModal);
 
-    return (
-        <Container sx={{
-            width: "100%",
-            height: "100%",
-            margin: "4rem auto"
-        }}>
-            <Box margin="4rem 0" textAlign="center" sx={{ color: "#5a5353" }}>
-                <Typography variant="h3" fontWeight="bold">
-                    Busca un doctor, agenda tu cita
-                </Typography>
-                <Typography variant="body1" sx={{ color: "#726969" }}>
-                    Los mejores doctores se adaptan a tus necesidades y horarios
-                </Typography>
-            </Box>
-            <Grid2 container columnGap={4}>
-                <MedicosFilters />
-                <Grid2 size="grow">
-                    <MedicosGrid />
-                </Grid2>
-            </Grid2>
-            {
-                showModal && (
-                    <CustomModal>
-                        <ModalMenu />
-                    </CustomModal>
-                )
-            }
-        </Container>
-    );
+  return (
+    <Container
+      sx={{
+        width: "100%",
+        height: "100%",
+        margin: "4rem auto",
+      }}
+    >
+      <Box margin="4rem 0" textAlign="center" sx={{ color: "#5a5353" }}>
+        <Typography variant="h3" fontWeight="bold">
+          Busca un doctor, agenda tu cita
+        </Typography>
+        <Typography variant="body1" sx={{ color: "#726969" }}>
+          Los mejores doctores se adaptan a tus necesidades y horarios
+        </Typography>
+      </Box>
+      <Grid2 container spacing={1} columns={{ xs: 12, sm: 12, md: 12 }}>
+        <MedicosFilters />
+        <Grid2 size={{ xs: 12, sm: 8, md: 9 }}>
+          <MedicosGrid />
+        </Grid2>
+      </Grid2>
+      {showModal && (
+        <CustomModal>
+          <ModalMenu />
+        </CustomModal>
+      )}
+    </Container>
+  );
 };
--- frontend/src/components/sections/MedicosDisponibles/MedicosGrid/MedicosGrid.tsx
+++ frontend/src/components/sections/MedicosDisponibles/MedicosGrid/MedicosGrid.tsx
@@ -4,37 +4,32 @@ import { CustomError } from "@tipos/types";
 import { useUserStore } from "@store/user.store";
 import { useState, useEffect } from "react";
 import { getMedicos } from "@services/medic/getMedicos";
-import { Box } from "@mui/material";
+import { Grid2 } from "@mui/material";
 import { GridLoading } from "./GridLoading";
 import { GridError } from "./GridError";
 
 export const MedicosGrid = () => {
-    const [medicos, setMedicos] = useState<MedicoData[]>([]);
-    const [error, setError] = useState<CustomError>(null);
-    const [loading, setLoading] = useState(false);
-    const token = useUserStore(state => state.getToken)();
+  const [medicos, setMedicos] = useState<MedicoData[]>([]);
+  const [error, setError] = useState<CustomError>(null);
+  const [loading, setLoading] = useState(false);
+  const token = useUserStore((state) => state.getToken)();
 
-    useEffect(() => {
-        getMedicos({ token, setDataRows: setMedicos, setLoading, setError });
-    }, []);
+  useEffect(() => {
+    getMedicos({ token, setDataRows: setMedicos, setLoading, setError });
+  }, []);
 
+  if (loading) {
+    return <GridLoading />;
+  }
 
-    if (loading) {
-        return <GridLoading />;
-    }
-
-    if (error) {
-        return <GridError description={error.description} />;
-    }
-    return (
-        <Box display="grid" gap=".5rem" gridTemplateColumns="repeat(auto-fill,minmax(200px, 1fr))">
-            {
-                !error && !loading && (
-                    medicos.map((doctor) => (
-                        <MedicCard key={doctor.id} doctor={doctor} />
-                    ))
-                )
-            }
-        </Box>
-    );
-}
+  if (error) {
+    return <GridError description={error.description} />;
+  }
+  return (
+    <Grid2 container spacing={1} columns={{ xs: 12, sm: 12, md: 12 }}>
+      {medicos.map((doctor) => (
+        <MedicCard key={doctor.id} doctor={doctor} />
+      ))}
+    </Grid2>
+  );
+};
