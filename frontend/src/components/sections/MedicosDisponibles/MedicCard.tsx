fba80d4ad542f58fa030a84f83994a28c1edb574
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-04-22 11:51:05 -0300

    feat: se mejoro el responsive de la vista medicos disponibles

--- frontend/src/components/sections/MedicosDisponibles/MedicCard.tsx
+++ frontend/src/components/sections/MedicosDisponibles/MedicCard.tsx
@@ -1,41 +1,50 @@
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Grid2, Typography } from "@mui/material";
 import { MedicoData } from "@tipos/backendTypes";
 import { EspecialidadesMedicas } from "@tipos/component";
 import { useNavigate } from "react-router";
 
 interface DoctorCardProps {
-	doctor: MedicoData;
+  doctor: MedicoData;
 }
 
 export const MedicCard = ({ doctor }: DoctorCardProps) => {
-	const { id, name, lastname, speciality } = doctor;
-	const fullname = `${name} ${lastname}`;
+  const { id, name, lastname, speciality } = doctor;
+  const fullname = `${name} ${lastname}`;
 
-	const navigate = useNavigate();
-	const handleClick = () => {
-		navigate(`/medico/${id}`);
-	};
-	return (
-		<Box
-			key={id}
-			sx={{
-				backgroundColor: "#fff",
-				textAlign: "center",
-				border: "1px solid #c1c1c1",
-				padding: "20px 5px",
-				cursor: "pointer",
-				height: "250px",
-				alignContent: "center"
-			}}
-			onClick={handleClick}
-		>
-			<Avatar alt={fullname} src="/assets/img/user.svg" sx={{ margin: "auto", width: 120, height: 120, border: "1px solid #c1c1c1" }} />
-			<Typography fontSize="1.2rem" sx={{ color: "#726969"}}>
-				{fullname}
-			</Typography>
-			<Typography fontSize="1rem" color="gray" textTransform="capitalize">
-				{EspecialidadesMedicas[speciality]}
-			</Typography>
-		</Box>
-	);
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/medico/${id}`);
+  };
+  return (
+    <Grid2
+	size={{ xs: 12, sm: 6, md: 4 }}
+      sx={{
+        backgroundColor: "#fff",
+        textAlign: "center",
+        border: "1px solid #c1c1c1",
+        padding: "20px 5px",
+        cursor: "pointer",
+        height: "250px",
+        alignContent: "center",
+      }}
+      onClick={handleClick}
+    >
+      <Avatar
+        alt={fullname}
+        src="/assets/img/user.svg"
+        sx={{
+          margin: "auto",
+          width: 120,
+          height: 120,
+          border: "1px solid #c1c1c1",
+        }}
+      />
+      <Typography fontSize="1.2rem" sx={{ color: "#726969" }}>
+        {fullname}
+      </Typography>
+      <Typography fontSize="1rem" color="gray" textTransform="capitalize">
+        {EspecialidadesMedicas[speciality]}
+      </Typography>
+    </Grid2>
+  );
 };
--- frontend/src/components/sections/MedicosDisponibles/MedicosFilters/MedicosFilters.tsx
+++ frontend/src/components/sections/MedicosDisponibles/MedicosFilters/MedicosFilters.tsx
@@ -1,16 +1,34 @@
-import { Grid2 } from "@mui/material";
+import { Grid2, IconButton, useMediaQuery } from "@mui/material";
 import { SelectorEspecialidad } from "./SelectorEspecialidad";
 import { BarraBusqueda } from "./BarraBusqueda";
 import { FiltrosSelected } from "./FiltrosSelected";
 import { GeneroRadioButton } from "./GeneroRadioButton";
+import { useState } from "react";
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 
 export const MedicosFilters = () => {
-    return (
-        <Grid2 width="300px" padding="1em 1.5em" bgcolor="#fff" borderRadius="10px">
-            <BarraBusqueda />
-            <FiltrosSelected />
-            <GeneroRadioButton />
-            <SelectorEspecialidad />
-        </Grid2>
-    );
-}
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const [openFilterMenu, setOpenFilterMenu] = useState(false);
+  return (
+    <>
+      {isMobile && (
+        <IconButton onClick={() => setOpenFilterMenu(!openFilterMenu)}>
+          <FilterAltIcon color="primary"/>
+        </IconButton>
+      )}
+      <Grid2
+        size={{ xs: 12, sm: 4, md: 3 }}
+        padding={isMobile && !openFilterMenu ? "0px" : "1em 1.5em"}
+        bgcolor="#fff"
+        borderRadius="10px"
+        overflow={"hidden"}
+        height={isMobile && !openFilterMenu ? "0px" : "min-content"}
+      >
+        <BarraBusqueda />
+        <FiltrosSelected />
+        <GeneroRadioButton />
+        <SelectorEspecialidad />
+      </Grid2>
+    </>
+  );
+};
