a730abb16e3f0d21f6d6e796d443a959800d28aa
fr4ncisx <fr4ncisx@gmail.com>
2025-04-26 02:15:50 -0300

    feat: added configuration for caffeine

---
+++ back/healthapp/src/main/java/com/healthcare/domain/configuration/CacheConfig.java
@@ -0,0 +1,37 @@
+package com.healthcare.domain.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Value("${cache.expiration.time:600}")
+    private long EXPIRATION_TIME;
+
+    @Value("${cache.max.elements:100}")
+    private long MAX_ELEMENTS;
+
+    @Bean
+    Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(EXPIRATION_TIME, TimeUnit.SECONDS)
+                .initialCapacity(10)
+                .maximumSize(MAX_ELEMENTS);
+    }
+
+    @Bean
+    CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
