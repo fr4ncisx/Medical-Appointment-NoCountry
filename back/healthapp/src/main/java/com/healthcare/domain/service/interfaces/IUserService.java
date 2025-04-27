368b7c48c2380031f5c600f1fe8ed5640fdaa14a
fr4ncisx <fr4ncisx@gmail.com>
2025-04-27 17:28:14 -0300

    fix: updated parameters from getUser and edit, we are no longer requesting HttpServletRequest to access token

--- back/healthapp/src/main/java/com/healthcare/domain/service/interfaces/IUserService.java
+++ back/healthapp/src/main/java/com/healthcare/domain/service/interfaces/IUserService.java
@@ -2,9 +2,8 @@ package com.healthcare.domain.service.interfaces;
 
 import com.healthcare.domain.dto.request.UserRequestUpdate;
 import com.healthcare.domain.dto.response.UserResponse;
-import jakarta.servlet.http.HttpServletRequest;
 
 public interface IUserService {
-    UserResponse getUser(String email, HttpServletRequest request);
-    void edit(Long id, UserRequestUpdate user, HttpServletRequest request);
+    UserResponse getUser(String email);
+    void edit(Long id, UserRequestUpdate user);
 }
