33f7c21d728e48855986bb913c2ba3efe43e9c9c
fr4ncisx <fr4ncisx@gmail.com>
2025-05-02 17:31:16 -0300

    feat: updated dto with result response for deletions with non null includes

--- back/healthapp/src/main/java/com/healthcare/domain/dto/response/CloudinaryResponse.java
+++ back/healthapp/src/main/java/com/healthcare/domain/dto/response/CloudinaryResponse.java
@@ -1,5 +1,6 @@
 package com.healthcare.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,7 +9,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CloudinaryResponse {
     @JsonProperty("url")
     private String secure_url;
+    private String result;
 }
