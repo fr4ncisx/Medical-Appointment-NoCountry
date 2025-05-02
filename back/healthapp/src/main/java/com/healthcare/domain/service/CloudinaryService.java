8f34905ec798b710746cac90a77e7881174b3011
fr4ncisx <fr4ncisx@gmail.com>
2025-05-02 17:30:45 -0300

    feat: added delete logic to delete image from cloudinary

--- back/healthapp/src/main/java/com/healthcare/domain/service/CloudinaryService.java
+++ back/healthapp/src/main/java/com/healthcare/domain/service/CloudinaryService.java
@@ -3,6 +3,7 @@ package com.healthcare.domain.service;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.healthcare.domain.dto.response.CloudinaryResponse;
+import com.healthcare.domain.exceptions.CloudinaryException;
 import com.healthcare.domain.exceptions.IOFileException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,7 +31,7 @@ public class CloudinaryService {
                     .upload(file, ObjectUtils.asMap("public_id", "healthapp/" + file.getName().trim().replaceFirst("[.][^.]+$", "")))
                     .get("secure_url")
                     .toString();
-            return new CloudinaryResponse(json);
+            return new CloudinaryResponse(json, null);
         } catch (IOException e) {
             throw new IOFileException("Error uploading file: " + e.getMessage());
         } finally {
@@ -39,6 +40,18 @@ public class CloudinaryService {
             } catch (IOException deleteEx) {
                 log.warn("Could not delete file {}: {}", file.getAbsolutePath(), deleteEx.getMessage());
             }
+        }
+    }
+
+    public CloudinaryResponse deleteFromCloudinary(String publicId) {
+        try {
+            var json = cloudinary.uploader()
+                    .destroy(publicId, ObjectUtils.emptyMap())
+                    .get("result")
+                    .toString();
+            return new CloudinaryResponse(null, json);
+        } catch (Exception e) {
+            throw new CloudinaryException(e.getMessage());
         }
     }
 }
