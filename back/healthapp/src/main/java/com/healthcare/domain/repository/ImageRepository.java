ccf1d426d8d21eb4dc86739e0d668a243ab98acb
fr4ncisx <fr4ncisx@gmail.com>
2025-05-02 16:37:18 -0300

    feat: added ImageRepository

---
+++ back/healthapp/src/main/java/com/healthcare/domain/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.healthcare.domain.repository;
+
+import com.healthcare.domain.model.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
