18056ff8480c1fbe5874c150aa460873b2dba8f3
fr4ncisx <fr4ncisx@gmail.com>
2025-05-02 16:39:41 -0300

    feat: updated medic creation now request an image url to create a medic

--- back/healthapp/src/main/java/com/healthcare/domain/service/MedicServiceImpl.java
+++ back/healthapp/src/main/java/com/healthcare/domain/service/MedicServiceImpl.java
@@ -2,16 +2,19 @@ package com.healthcare.domain.service;
 
 import com.healthcare.domain.dto.request.MedicRequest;
 import com.healthcare.domain.dto.request.MedicRequestUpdate;
+import com.healthcare.domain.dto.request.UserRequest;
 import com.healthcare.domain.dto.response.MedicResponse;
 import com.healthcare.domain.dto.response.MedicUserResponse;
 import com.healthcare.domain.exceptions.DuplicatedEntryEx;
 import com.healthcare.domain.exceptions.MedicDeletionException;
 import com.healthcare.domain.exceptions.MedicNotFoundException;
 import com.healthcare.domain.exceptions.NotFoundInDatabaseException;
+import com.healthcare.domain.model.entity.Image;
 import com.healthcare.domain.model.entity.Medic;
 import com.healthcare.domain.model.entity.User;
 import com.healthcare.domain.model.enums.Role;
 import com.healthcare.domain.repository.AppointmentRepository;
+import com.healthcare.domain.repository.ImageRepository;
 import com.healthcare.domain.repository.MedicRepository;
 import com.healthcare.domain.repository.UserRepository;
 import com.healthcare.domain.service.interfaces.IMedicService;
@@ -28,8 +31,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -38,6 +43,7 @@ public class MedicServiceImpl implements IMedicService {
     private final MedicRepository medicRepository;
     private final AppointmentRepository appointmentRepository;
     private final UserRepository userRepository;
+    private final ImageRepository imageRepository;
     private final ModelMapper modelMapper;
     private final BCryptPasswordEncoder passwordEncoder;
 
@@ -87,7 +93,7 @@ public class MedicServiceImpl implements IMedicService {
     @Override
     @Transactional
     public ResponseEntity<Map<String, Object>> createMedic(MedicRequest medicRequest) {
-        var userRequest = medicRequest.getUser();
+        UserRequest userRequest = medicRequest.getUser();
         if (medicRepository.existsByDocumentId(medicRequest.getDocumentId())) {
             throw new DuplicatedEntryEx("Médico ya registrado");
         }
@@ -95,9 +101,12 @@ public class MedicServiceImpl implements IMedicService {
             throw new DuplicatedEntryEx("El correo ya esta asociado a una cuenta");
         }
 
-        var userEncodedPassword = passwordEncoder.encode(userRequest.getPassword());
+        var imageRequest = medicRequest.getImage();
+        imageRequest.setDate(LocalDate.now());
+        Image image = modelMapper.map(imageRequest, Image.class);
+        String userEncodedPassword = passwordEncoder.encode(userRequest.getPassword());
         User user = new User(userRequest, userEncodedPassword, Role.MEDICO);
-        Medic medic = new Medic(medicRequest, user);
+        Medic medic = new Medic(medicRequest, user, image);
 
         medicRepository.save(medic);
         return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
@@ -133,7 +142,15 @@ public class MedicServiceImpl implements IMedicService {
     public void edit(Long id, MedicRequestUpdate medicRequest) {
         var medic = medicRepository.findById(id)
                 .orElseThrow(() -> new NotFoundInDatabaseException("No se encontró el médico"));
-        modelMapper.map(medicRequest, medic);
+        Optional.ofNullable(medic.getImage())
+                .ifPresent(imageRepository::delete);
+
+        var imageRequest = medicRequest.getImage();
+        imageRequest.setDate(LocalDate.now());
+
+        Image image = modelMapper.map(imageRequest, Image.class);
+        medic.setImage(image);
+        modelMapper.map(medicRequest, Medic.class);
         medicRepository.save(medic);
     }
 }
