1cbf0517ff350b280901d376c7aba950269e75b4
fr4ncisx <fr4ncisx@gmail.com>
2025-05-02 23:03:22 -0300

    feat: updated CloudinaryResponse adding public_id property and ignoring all other properties

--- back/healthapp/src/main/java/com/healthcare/domain/dto/response/CloudinaryResponse.java
+++ back/healthapp/src/main/java/com/healthcare/domain/dto/response/CloudinaryResponse.java
@@ -1,5 +1,6 @@
 package com.healthcare.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class CloudinaryResponse {
     @JsonProperty("url")
     private String secure_url;
+    private String public_id;
     private String result;
 }
