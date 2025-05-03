893a6c398f326e1940d92ab01dc560d5f8177ae6
fr4ncisx <fr4ncisx@gmail.com>
2025-05-02 23:03:57 -0300

    feat: added converter to url to public_id and serialize responses to DTO

--- back/healthapp/src/main/java/com/healthcare/domain/service/CloudinaryService.java
+++ back/healthapp/src/main/java/com/healthcare/domain/service/CloudinaryService.java
@@ -2,19 +2,22 @@ package com.healthcare.domain.service;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.healthcare.domain.dto.response.CloudinaryResponse;
 import com.healthcare.domain.exceptions.CloudinaryException;
 import com.healthcare.domain.exceptions.IOFileException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Map;
 
 @Slf4j
-@Service
+@Component
 @RequiredArgsConstructor
 public class CloudinaryService {
     private final Cloudinary cloudinary;
@@ -28,10 +31,10 @@ public class CloudinaryService {
     public CloudinaryResponse uploadToCloudinary(File file) {
         try {
             var json = cloudinary.uploader()
-                    .upload(file, ObjectUtils.asMap("public_id", "healthapp/" + file.getName().trim().replaceFirst("[.][^.]+$", "")))
-                    .get("secure_url")
-                    .toString();
-            return new CloudinaryResponse(json, null);
+                    .upload(file, ObjectUtils.asMap("public_id", "healthapp/" + file.getName()
+                            .trim()
+                            .replaceFirst("[.][^.]+$", "")));
+            return serializeResponse(json);
         } catch (IOException e) {
             throw new IOFileException("Error uploading file: " + e.getMessage());
         } finally {
@@ -46,12 +49,24 @@ public class CloudinaryService {
     public CloudinaryResponse deleteFromCloudinary(String publicId) {
         try {
             var json = cloudinary.uploader()
-                    .destroy(publicId, ObjectUtils.emptyMap())
-                    .get("result")
-                    .toString();
-            return new CloudinaryResponse(null, json);
+                    .destroy(publicId, ObjectUtils.emptyMap());
+            return serializeResponse(json);
         } catch (Exception e) {
             throw new CloudinaryException(e.getMessage());
         }
+    }
+
+
+    public String convertUrlToPublicId(String url) {
+        if (url == null || url.isBlank())
+            return "";
+        String fixedUrl = url.replaceFirst(".*/upload/(v\\d+/)?", "");
+
+        return fixedUrl.replaceFirst("\\.[^.]+$", "");
+    }
+
+    public CloudinaryResponse serializeResponse(Map<String, Object> json){
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.convertValue(json,CloudinaryResponse.class );
     }
 }
