2a1b48df9aedff5b513d0a6446bb0cd1b8da6a00
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-05-06 14:27:04 -0300

    chore: add tanstack query library

--- frontend/src/components/layout/ProviderWrapper.tsx
+++ frontend/src/components/layout/ProviderWrapper.tsx
@@ -1,19 +1,26 @@
-import { ThemeProvider } from '@mui/material';
-import { theme } from '@ui/muiOverrideTheme';
-import { ReactNode } from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { ThemeProvider } from "@mui/material";
+import { theme } from "@ui/muiOverrideTheme";
+import { ReactNode } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
 import { es } from "date-fns/locale/es";
-import { esES } from '@mui/x-date-pickers/locales';
+import { esES } from "@mui/x-date-pickers/locales";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const spanishLocale = esES.components.MuiLocalizationProvider.defaultProps.localeText;
+const spanishLocale =
+  esES.components.MuiLocalizationProvider.defaultProps.localeText;
+const queryClient = new QueryClient();
 
 export const ProviderWrapper = ({ children }: { children: ReactNode }) => {
-    return (
-        <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={es} localeText={spanishLocale}>
-            <ThemeProvider theme={theme}>
-                {children}
-            </ThemeProvider>
-        </LocalizationProvider>
-    );
-}
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LocalizationProvider
+        dateAdapter={AdapterDateFns}
+        adapterLocale={es}
+        localeText={spanishLocale}
+      >
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </LocalizationProvider>
+    </QueryClientProvider>
+  );
+};
