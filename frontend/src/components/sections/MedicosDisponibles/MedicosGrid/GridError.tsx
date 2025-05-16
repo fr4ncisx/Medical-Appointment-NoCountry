e32400a7d6661a94d352e0d4398a5cccae1aeea1
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2025-05-15 21:59:11 -0300

    feat: se agrego funcionalidad al sistema de filtros

--- frontend/src/components/sections/MedicosDisponibles/MedicosGrid/GridError.tsx
+++ frontend/src/components/sections/MedicosDisponibles/MedicosGrid/GridError.tsx
@@ -8,7 +8,7 @@ export const GridError = ({ description }: Props) => {
     return (
         <Box textAlign="center" height="calc(100vh - 80px)">
             <Box sx={{ backgroundColor: "#ff000015", borderRadius: "10px", padding: "1rem" }}>
-                <Typography color="error" variant="h4">
+                <Typography color="error" variant="h5">
                     {`Ocurrio un error: ${description}`}
                 </Typography>
             </Box>
